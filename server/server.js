51022d1831b59ed6a9ee165895d109d9bb283e8b
unknown <jadhavsanket621@gmail.com>
2025-03-29 01:45:58 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
-const { Socket } = require('socket.io-client');
+require('dotenv').config();
+const { Server } = require('socket.io');
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
 
-const io = require('socket.io')(3001,{
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3001;
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: FRONTEND_URL,
         methods: ['GET', 'POST'],
     },
 });
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
 
-    socket.on('get-document', documentId => {
-        const data = ""
-        socket.join(documentId)
-        socket.emit('load-document', data)
+    socket.on('get-document', (documentId) => {
+        const data = '';
+        socket.join(documentId);
+        socket.emit('load-document', data);
 
-        socket.on('send-changes', delta => {
-            socket.broadcast.to(documentId).emit('receive-changes', delta)
-        })
-    })
-})
+        socket.on('send-changes', (delta) => {
+            socket.broadcast.to(documentId).emit('receive-changes', delta);
+        });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Socket.IO server is running');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
